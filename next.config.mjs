209046185e71c46c28e2eb9a55dd8ad877aca91a
anagri <127566+anagri@users.noreply.github.com>
2024-09-10 21:02:03 +0530

    [Amir] converting to pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  register: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  transpilePackages: ["geist"],
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default withPWA(nextConfig);
